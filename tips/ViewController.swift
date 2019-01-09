33eb708937a7771e552b469e32557e32d15d0906
blinkous <saradhani1@gmail.com>
2019-01-09 14:29:47 -0500

    trying to add settings

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.billField.becomeFirstResponder()
@@ -39,5 +39,10 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
+    
+    @IBAction func defaultTip(_ sender: Any) {
+        
+    }
+    
 }
 
